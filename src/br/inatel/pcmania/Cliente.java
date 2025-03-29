e2a9ee6a8a3e39b17a8289ffc5408ad32038a357
matheusandrade23 <matheus.andrade@nextdevices.com.br>
2025-03-29 15:02:31 -0300

    feat: cliente methods

--- src/br/inatel/pcmania/Cliente.java
+++ src/br/inatel/pcmania/Cliente.java
@@ -2,6 +2,15 @@ class Cliente {
   private String nome;
   private long cpf;
 
+  private Computador[] carrinho;
+
+  public Cliente(String nome, long cpf){
+    this.nome = nome;
+    this.cpf = cpf;
+
+    carrinho =  new Computador[10];
+  }
+
   public float calculaTotalCompra(){
     return 1;
   }
