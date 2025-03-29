7a8627773c21bb7c3e37857a6a1f29891cbd92fa
matheusandrade23 <matheus.andrade@nextdevices.com.br>
2025-03-29 14:56:12 -0300

    feat: add computador & relations

---
+++ src/br/inatel/pcmania/Computador.java
@@ -0,0 +1,46 @@
+class Computador {
+  private String marca;
+  private float preco;
+
+  private MemoriaUSB[] memoriasUsb;
+
+  private SistemaOperacional sistemaOperacional;
+  private HardwareBasico hardwareBasico;
+
+  public Computador(
+    String marca, 
+    float preco, 
+    String nomeProcessador, 
+    int capacidadeProcessador,
+    int memoriaRamGB,
+    int armazenamentoHDGB,
+    String nomeSistemaOperacional,
+    int tipoSistemaOperacional
+  ){
+    this.marca = marca;
+    this.preco = preco;
+
+    hardwareBasico = new HardwareBasico(nomeProcessador, capacidadeProcessador, armazenamentoHDGB, memoriaRamGB);
+    sistemaOperacional = new SistemaOperacional(nomeSistemaOperacional, tipoSistemaOperacional);
+
+    memoriasUsb = new MemoriaUSB[1];
+  }
+
+  public void addMemoriaUSB(MemoriaUSB musb){
+    if(memoriasUsb.length == 1){
+      System.err.println("O PC já possui uma memória USB!");
+      return;
+    }
+
+    memoriasUsb[1] = musb;
+  }
+
+  public void mostraPCConfigs(){
+    System.out.println("Informações Gerais: " + hardwareBasico.getNome() + " - Clock Processador: " + hardwareBasico.getCapacidade() + "Mhz");
+    System.out.println("Sistema Operacional: " + sistemaOperacional.getNome() + " (" + sistemaOperacional.getTipo() + " bits)");
+
+    if(memoriasUsb.length == 1) {
+      System.out.println("Acompanha: Pen Drive " + memoriasUsb[1].getNome() + " " + memoriasUsb[1].getCapacidade());
+    }
+  }
+}
