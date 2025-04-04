0c7d6199cd0354ebacf5a629ec6e3b8700f21cb5
matheusandrade23 <matheus.andrade@nextdevices.com.br>
2025-04-03 22:23:00 -0300

    fix: format condition

--- src/br/inatel/pcmania/formatadores/Formatadores.java
+++ src/br/inatel/pcmania/formatadores/Formatadores.java
@@ -2,7 +2,7 @@ package br.inatel.pcmania.formatadores;
 
 public class Formatadores{
   public static String formatarMemoria(float capacidadeGB){
-    if(capacidadeGB > 1000){
+    if(capacidadeGB >= 1000){
       return capacidadeGB/1000 + " TB";
     }
 
